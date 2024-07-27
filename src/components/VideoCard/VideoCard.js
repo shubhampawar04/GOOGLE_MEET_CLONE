38124ffc8a9bc018ce23595e7c035b994cf332df
shubhartika0414 <pawarshubham81668@gmail.com>
2024-07-27 18:38:36 +0530

    add photos

--- src/components/VideoCard/VideoCard.js
+++ src/components/VideoCard/VideoCard.js
@@ -2,6 +2,16 @@
 import React from 'react';
 import "./VideoCard.css"
 import Shubham from "../../assets/1707758181035.jpg"
+import sakshi from "../../assets/sakshi.jpeg"
+import smita from "../../assets/smita.jpeg"
+import kirti from "../../assets/kirti.jpeg"
+import vishal from "../../assets/vishal.jpeg"
+import rutik from "../../assets/rutik.jpeg"
+import dhiraj from "../../assets/dhiraj.jpeg"
+import sangram from "../../assets/sangram.jpeg"
+import saurav from "../../assets/saurav.jpeg"
+import akash from "../../assets/akash.jpeg"
+import nisha from "../../assets/nisha.jpeg"
 import SurajSir from "../../assets/suraj.png";
 import OnMic from "../../assets/on-mic.png"
 import OffMic from "../../assets/off-mic.png"
@@ -19,52 +29,52 @@ const VideoCard = () => {
        
       </div>
       <div className="video-tile">
-        <img src='https://i.pravatar.cc/400?img=32' className='profile-img'/>
+        <img src= {sakshi} className='profile-img'/>
         <img src={OffMic} className='mic-img'/>
         <span className='students-name'>Sakshi 🩷</span>
       </div>
       <div className="video-tile">
-        <img src='https://i.pravatar.cc/150?img=27' className='profile-img'/>
+        <img src= {smita} className='profile-img'/>
         <img src={OffMic} className='mic-img'/>
         <span className='students-name'>Smita 🧡</span>
       </div>
          <div className="video-tile">
-        <img src='https://i.pravatar.cc/400?img=47' className='profile-img'/>
+        <img src= {kirti} className='profile-img'/>
         <img src={OffMic} className='mic-img'/>
         <span className='students-name'>Kirti 💛</span>
       </div>
          <div className="video-tile">
-        <img src='https://i.pravatar.cc/400?img=33' className='profile-img'/>
+        <img src= {vishal} className='profile-img'/>
         <img src={OffMic} className='mic-img'/>
         <span className='students-name'>Vishal 💜</span>
       </div>
          <div className="video-tile">
-        <img src='https://i.pravatar.cc/400?img=52' className='profile-img'/>
+        <img src= {rutik} className='profile-img'/>
         <img src={OffMic} className='mic-img'/>
         <span className='students-name'>Rutik 💝</span>
       </div>
          <div className="video-tile">
-        <img src='https://i.pravatar.cc/400?img=7' className='profile-img'/>
+        <img src= {dhiraj} className='profile-img'/>
         <img src={OffMic} className='mic-img'/>
         <span className='students-name'>Dhiraj 💕</span>
       </div>
          <div className="video-tile">
-        <img src='https://i.pravatar.cc/400?img=55' className='profile-img'/>
+        <img src= {sangram} className='profile-img'/>
         <img src={OffMic} className='mic-img'/>
         <span className='students-name'>Sangram 🩵</span>
       </div>
          <div className="video-tile">
-        <img src='https://i.pravatar.cc/400?img=54' className='profile-img'/>
+        <img src= {saurav} className='profile-img'/>
         <img src={OffMic} className='mic-img'/>
         <span className='students-name'>Saurav 🤍</span>
       </div>
          <div className="video-tile">
-        <img src='https://i.pravatar.cc/400?img=56' className='profile-img'/>
+        <img src= {akash} className='profile-img'/>
         <img src={OffMic} className='mic-img'/>
         <span className='students-name'>Akash ❤️‍🩹</span>
       </div>
          <div className="video-tile">
-        <img src='https://i.pravatar.cc/400?img=44' className='profile-img'/>
+        <img src= {nisha} className='profile-img'/>
         <img src={OffMic} className='mic-img'/>
         <span className='students-name'>Nisha ❣️</span>
       </div>
